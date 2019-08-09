286f0eb6c3685f00e94ec005ab566caf4721e657
Jeff King <jeff@Jeffde-MBP.Home>
2019-08-09 19:22:17 +0800

    add GetMemeWithoutTags api

--- app/api.go
+++ app/api.go
@@ -26,3 +26,22 @@ func GetMemeDetails(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "can't write json string to response", http.StatusBadRequest)
 	}
 }
+
+// GetMemeWithoutTags api func to return meme details which has no tags
+func GetMemeWithoutTags(w http.ResponseWriter, r *http.Request) {
+	db, connectErr := connectDB()
+	if connectErr != nil {
+		log.Println(connectErr.Error())
+		http.Error(w, "connect db error", http.StatusBadRequest)
+	}
+
+	limit := 100
+	memes, _ := getMemesWithoutAbout(db, limit)
+	jsonString, _ := json.Marshal(memes)
+
+	if _, err := w.Write(jsonString); err != nil {
+		log.Println(err.Error())
+		log.Printf("json content:\n %s\n", jsonString)
+		http.Error(w, "can't write json string to response", http.StatusBadRequest)
+	}
+}
