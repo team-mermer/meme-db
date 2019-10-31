98e03c26af2d616110d86e4489eab0062d566e81
Jeff King <goan15910@gmail.com>
2019-10-31 21:28:04 +0800

    move serving wrapper to app

--- main.go
+++ main.go
@@ -1,46 +1,24 @@
 package main
 
 import (
-	"database/sql"
-	"io/ioutil"
-	"log"
 	"meme-db/app"
 	"net/http"
 	"os"
-
-	"gopkg.in/yaml.v2"
 )
 
-func serveWrapper(config app.DBConfig, serveFunc func(*sql.DB, http.ResponseWriter, *http.Request)) http.Handler {
-	db, err := app.ConnectDB(config)
-	if err != nil {
-		log.Println(err.Error())
-	}
-
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		serveFunc(db, w, r)
-	})
-}
-
 func main() {
-	data, err := ioutil.ReadFile(os.Args[1])
+	config, err := app.GetDBConfig(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
 
-	var config app.DBConfig
-	if err := yaml.Unmarshal(data, &config); err != nil {
-		log.Fatalf("error: %v", err)
-	}
-	log.Printf("Get config: %v\n", config)
-
 	// add func to handle url request
-	http.Handle("/get_meme_details", serveWrapper(config, app.GetMemeDetails))
-	http.Handle("/get_trending_memes", serveWrapper(config, app.GetTrendingMemes))
-	http.Handle("/get_meme_without_tags", serveWrapper(config, app.GetMemeWithoutTags))
-	http.Handle("/insert_meme_without_tags", serveWrapper(config, app.InsertMemeWithoutTags))
-	http.Handle("/insert_meme_abouts_and_tags", serveWrapper(config, app.InsertMemeAboutsAndTags))
-	http.Handle("/increment_meme_click", serveWrapper(config, app.IncrementMemeClick))
+	http.Handle("/get_meme_details", app.ServeWrapper(config, app.GetMemeDetails))
+	http.Handle("/get_trending_memes", app.ServeWrapper(config, app.GetTrendingMemes))
+	http.Handle("/get_meme_without_tags", app.ServeWrapper(config, app.GetMemeWithoutTags))
+	http.Handle("/insert_meme_without_tags", app.ServeWrapper(config, app.InsertMemeWithoutTags))
+	http.Handle("/insert_meme_abouts_and_tags", app.ServeWrapper(config, app.InsertMemeAboutsAndTags))
+	http.Handle("/increment_meme_click", app.ServeWrapper(config, app.IncrementMemeClick))
 
 	// listen and serve
 	if err := http.ListenAndServe(":8070", nil); err != nil {
