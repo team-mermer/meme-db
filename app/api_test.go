32c7f7fd3e68104a207bd2fedf2479655c8d8b22
Jeff King <goan15910@gmail.com>
2019-10-31 22:10:12 +0800

    add template for insert apis

--- app/api_test.go
+++ app/api_test.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -9,8 +10,16 @@ import (
 
 const testConfigPath = "../test_config.yaml"
 
-func testAPIUtils(method string, APIUrl string, handler http.Handler, t *testing.T) string {
-	req, err := http.NewRequest(method, APIUrl, nil)
+func testAPIUtils(
+	method string,
+	APIUrl string,
+	handler http.Handler,
+	t *testing.T,
+	expectedString string,
+	bodyBuffer *bytes.Buffer,
+) {
+
+	req, err := http.NewRequest(method, APIUrl, bodyBuffer)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -25,7 +34,15 @@ func testAPIUtils(method string, APIUrl string, handler http.Handler, t *testing
 		)
 	}
 
-	return rr.Body.String()
+	APIBodyString := rr.Body.String()
+
+	if APIBodyString != expectedString {
+		t.Errorf(
+			"handler returned unexpected body: got %v want %v",
+			APIBodyString,
+			expectedString,
+		)
+	}
 }
 
 func TestGetMemeDetails(t *testing.T) {
@@ -34,7 +51,7 @@ func TestGetMemeDetails(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	handler := ServeWrapper(config, GetTrendingMemes)
+	handler := ServeWrapper(config, GetMemeDetails)
 
 	// expected result
 	expectMemes := make([]memeDetail, 0)
@@ -49,15 +66,13 @@ func TestGetMemeDetails(t *testing.T) {
 	expectedBytes, _ := json.Marshal(expectMemes)
 	expectedString := string(expectedBytes)
 
-	APIBodyString := testAPIUtils("GET", "/get_meme_details", handler, t)
-
-	if APIBodyString != expectedString {
-		t.Errorf(
-			"handler returned unexpected body: got %v want %v",
-			APIBodyString,
-			expectedString,
-		)
-	}
+	testAPIUtils(
+		"GET",
+		"/get_meme_details",
+		handler,
+		t,
+		expectedString,
+		nil)
 }
 
 func TestGetMemeWithoutTags(t *testing.T) {
@@ -66,7 +81,7 @@ func TestGetMemeWithoutTags(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	handler := ServeWrapper(config, GetTrendingMemes)
+	handler := ServeWrapper(config, GetMemeWithoutTags)
 
 	// expected result
 	expectMemes := make([]memeDetail, 0)
@@ -81,15 +96,13 @@ func TestGetMemeWithoutTags(t *testing.T) {
 	expectedBytes, _ := json.Marshal(expectMemes)
 	expectedString := string(expectedBytes)
 
-	APIBodyString := testAPIUtils("GET", "/get_meme_without_tags", handler, t)
-
-	if APIBodyString != expectedString {
-		t.Errorf(
-			"handler returned unexpected body: got %v want %v",
-			APIBodyString,
-			expectedString,
-		)
-	}
+	testAPIUtils(
+		"GET",
+		"/get_meme_without_tags",
+		handler,
+		t,
+		expectedString,
+		nil)
 }
 
 func TestGetTrendingMemes(t *testing.T) {
@@ -113,25 +126,132 @@ func TestGetTrendingMemes(t *testing.T) {
 	expectedBytes, _ := json.Marshal(expectMemes)
 	expectedString := string(expectedBytes)
 
-	APIBodyString := testAPIUtils("GET", "/get_trending_memes", handler, t)
-
-	if APIBodyString != expectedString {
-		t.Errorf(
-			"handler returned unexpected body: got %v want %v",
-			APIBodyString,
-			expectedString,
-		)
-	}
+	testAPIUtils(
+		"GET",
+		"/get_trending_memes",
+		handler,
+		t,
+		expectedString,
+		nil)
 }
 
 func TestInsertMemeWithoutTags(t *testing.T) {
 	// TODO @jeff: reset DB and insert example data
+	config, err := GetDBConfig(testConfigPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	handler := ServeWrapper(config, InsertMemeWithoutTags)
+
+	// expected result
+	expectMemes := make([]memeDetail, 0)
+	expectMemes = append(
+		expectMemes,
+		memeDetail{
+			ID:       0,
+			Title:    "chuan",
+			ImageURL: "www.blabla.com/blabla.jpg",
+			About:    "ark chuan",
+		})
+	expectedBytes, _ := json.Marshal(expectMemes)
+	expectedString := string(expectedBytes)
+
+	// compose url body
+	newMemes := make([]memeDetail, 0)
+	newMemes = append(
+		newMemes,
+		memeDetail{
+			ID:       0,
+			Title:    "chuan",
+			ImageURL: "www.blabla.com/blabla.jpg",
+			About:    "ark chuan",
+		})
+	jsonStr, _ := json.Marshal(newMemes)
+
+	testAPIUtils(
+		"GET",
+		"/insert_meme_without_tags",
+		handler,
+		t,
+		expectedString,
+		bytes.NewBuffer(jsonStr))
 }
 
 func TestInsertMemeAboutsAndTags(t *testing.T) {
 	// TODO @jeff: reset DB and insert example data
+	config, err := GetDBConfig(testConfigPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	handler := ServeWrapper(config, InsertMemeAboutsAndTags)
+
+	// expected result
+	expectMemes := make([]memeDetail, 0)
+	expectMemes = append(
+		expectMemes,
+		memeDetail{
+			ID:       0,
+			Title:    "chuan",
+			ImageURL: "www.blabla.com/blabla.jpg",
+			About:    "ark chuan",
+		})
+	expectedBytes, _ := json.Marshal(expectMemes)
+	expectedString := string(expectedBytes)
+
+	// compose url body
+	newMemes := make([]memeDetail, 0)
+	newMemes = append(
+		newMemes,
+		memeDetail{
+			ID:       0,
+			Title:    "chuan",
+			ImageURL: "www.blabla.com/blabla.jpg",
+			About:    "ark chuan",
+		})
+	jsonStr, _ := json.Marshal(newMemes)
+
+	testAPIUtils(
+		"GET",
+		"/insert_meme_abouts_and_tags",
+		handler,
+		t,
+		expectedString,
+		bytes.NewBuffer(jsonStr))
 }
 
 func TestIncrementMemeClick(t *testing.T) {
 	// TODO @jeff: reset DB and insert example data
+	config, err := GetDBConfig(testConfigPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	handler := ServeWrapper(config, InsertMemeWithoutTags)
+
+	// expected result
+	expectMemes := make([]memeDetail, 0)
+	expectMemes = append(
+		expectMemes,
+		memeDetail{
+			ID:       0,
+			Title:    "chuan",
+			ImageURL: "www.blabla.com/blabla.jpg",
+			About:    "ark chuan",
+		})
+	expectedBytes, _ := json.Marshal(expectMemes)
+	expectedString := string(expectedBytes)
+
+	// compose url body
+	IDs := make([]int, 0)
+	IDs = append(IDs, 0)
+	memeIDs := memeIDInput{IDs: IDs}
+	jsonStr, _ := json.Marshal(memeIDs)
+
+	testAPIUtils(
+		"GET",
+		"/increment_meme_click",
+		handler,
+		t,
+		expectedString,
+		bytes.NewBuffer(jsonStr),
+	)
 }
